7d667ac1d8bbefa65ef1b51e36e6da57962f6caf
biscaboy <djdlearn@gmail.com>
2018-02-02 01:29:23 -0700

    refactor: Update to initialization with Star class

--- js/app.js
+++ js/app.js
@@ -12,21 +12,45 @@
 * 	@description Tracks and displays player moves, stars and game timer.
 */
 const Scoreboard = function() {
-	this.counterElem = document.querySelector('.moves');
-	this.starsElem = document.querySelector('.stars');
-	this.timerElem = document.querySelector('.timer');
+	// keep references to the display Elements of the scoreboard and game over modal
+	this.counterElem = document.querySelector(this.CLASS_MOVES);
+	this.starsElem = document.querySelector(this.CLASS_STARS);
+	this.timerElem = document.querySelector(this.CLASS_TIMER);
+	this.modalRankElem = document.querySelector(this.CLASS_GAME_RANKING);
+	this.modalMovesElem = document.querySelector(this.CLASS_STATS_MOVES);
+	this.modalTimeElem = document.querySelector(this.CLASS_STATS_TIME);
+	this.modalStarsElem = document.querySelector(this.CLASS_STATS_STARS);
 	this.clockInterval = null;
 	this.moves = 0;
 	this.elapsedTime = 0;
 	this.matches = 0;
 }
 
+// static constants for the class
+Scoreboard.prototype.CLASS_MOVES = '.moves';
+Scoreboard.prototype.CLASS_STARS = '.stars';
+Scoreboard.prototype.CLASS_TIMER = '.timer';
+Scoreboard.prototype.CLASS_GAME_RANKING = '.game-ranking';
+Scoreboard.prototype.CLASS_STATS_MOVES = '.game-over-stats-moves';
+Scoreboard.prototype.CLASS_STATS_TIME = '.game-over-stats-time';
+Scoreboard.prototype.CLASS_STATS_STARS = '.game-over-stats-stars';
+Scoreboard.prototype.CLASS_GAME_OVER = 'game-over';
+Scoreboard.prototype.CLASS_MODAL_SHOW = 'modal-show';
+Scoreboard.prototype.CLASS_MODAL_SHOW_BG = 'modal-bg-show';
+Scoreboard.prototype.ZERO_ON_THE_CLOCK = '0:00';
+Scoreboard.prototype.RANK_FOUR = 'ninja';
+Scoreboard.prototype.RANK_THREE = 'master';
+Scoreboard.prototype.RANK_TWO = 'guru';
+Scoreboard.prototype.RANK_ONE = 'legend';
+
 /**
 * 	@description provides the elapsed time from the start of the game
 * 	@returns a formatted string in minutes and seconds (mm:ss).
 */
 Scoreboard.prototype.getGameDuration = function() {
+	// elapsedTime is in seconds so calculate the minutes
 	const mins = Math.floor(this.elapsedTime / 60);
+	// get and format the seconds
 	let secs = Math.floor(this.elapsedTime % 60);
 	if (secs < 10) {
 		secs = '0' + secs;
@@ -39,12 +63,13 @@ Scoreboard.prototype.getGameDuration = function() {
 *	displays it and updates it.
 */
 Scoreboard.prototype.startClock = function() {
+	// blank out the scoreboard and clear timer Interval one is running.
 	this.elapsedTime = 0;
 	this.matches = 0;
-	this.timerElem.innerHTML = '0:00';
+	this.timerElem.innerHTML = this.ZERO_ON_THE_CLOCK;
 	clearInterval(this.clockInterval);
+	// set up a timer that ticks at 1 second intervals and displays it.
 	this.clockInterval = setInterval(function(sb) {
-		// get the current duration time and display it
 		sb.elapsedTime += 1;
 		sb.timerElem.innerHTML = sb.getGameDuration();
 	}, 1000, this);
@@ -61,17 +86,18 @@ Scoreboard.prototype.stopClock = function() {
 * 	@description Updates the scoreboard with a move.
 *	A move consists of turing over two cards.
 */
-Scoreboard.prototype.incrememntMoves = function() {
+Scoreboard.prototype.incrementMoves = function() {
 	this.moves++;
 	this.counterElem.innerHTML = this.moves;
 	// remove stars depending on the players performance
 	// rules for removing stars
-	// 1.  wait at least 6 moves before checking, then check other move
-	// 2.  if the ratio of moves to matched cards is ever less than 25% remove a star
+	// 1.  wait at least 6 moves before checking, then check every other move
+	// 2.  if the ratio of moves to matched pairs is ever less than 25% remove a star
 	// document.querySelector('.success-ratio').innerHTML = Math.floor((this.matches / this.moves) * 100) + '%';
 	if (this.moves > 6 && this.moves % 2 == 0 && (this.matches / this.moves < 0.25)) {
-		const starToRemove = document.querySelector('.stars li');
+		const starToRemove = document.querySelector(Star.prototype.CLASS_SELECTOR);
 		if (starToRemove){
+			// TODO: insert an animation here.
 			starToRemove.parentElement.removeChild(starToRemove);
 		}
 	}
@@ -105,35 +131,66 @@ Scoreboard.prototype.startGame = function() {
 }
 
 /**
+* @description provide a ranking based on the ration of moves to pairs found
+* @return {string} the rank
+*/
+Scoreboard.prototype.getPlayerRank = function() {
+	let rank = this.RANK_ONE;
+	// TODO: update this to use the players success ratio.
+	const successRatio = (this.matches / this.moves) * 100;
+	if (successRatio >= 30) {
+		rank = this.RANK_TWO;
+	} else if (successRatio >= 20) {
+		rank = this.RANK_THREE;
+	} else if (successRatio >= 10) {
+		rank = this.RANK_FOUR;
+	}
+	return rank;
+}
+
+/**
 * 	@description  Stops the game clock, and displays a modal Congratulations message.
 */
 Scoreboard.prototype.stopGame = function () {
 	this.stopClock();
 	// display modal with winning stats.
-		let rank = 'trainee';
-	const stars = document.querySelectorAll('.stars li');
-	switch (stars.length) {
-		case 3:
-			rank = 'guru';
-			break;
-		case 2:
-			rank = 'master'
-			break;
-		case 1:
-			rank = 'ninja';
-	}
-	document.querySelector('.game-ranking').innerHTML = rank;
-	document.querySelector('.game-over-stats-moves').innerHTML = this.moves;
-	document.querySelector('.game-over-stats-time').innerHTML = this.getGameDuration();
-	document.querySelector('.game-over-stats-stars').innerHTML = stars.length;
-	cards.forEach(function(card){
-		card.elem.classList.add('game-over');
+	// insert the game stats into the modal
+	this.modalRankElem.innerHTML = this.getPlayerRank();
+	this.modalMovesElem.innerHTML = this.moves;
+	this.modalTimeElem.innerHTML = this.getGameDuration();
+	this.modalStarsElem.innerHTML = stars.length;
+
+	// animate the cards to show the game is over
+	cards.forEach(card => {
+		card.elem.classList.add(this.CLASS_GAME_OVER);
 	});
-	setTimeout(function(){
-		modalBg.classList.add('modal-bg-show');
-		modal.classList.add('modal-show');
+	// after 1.5 sec of animation above, show the modal on a dark background.
+	setTimeout(() => {
+		modalBg.classList.add(this.CLASS_MODAL_SHOW_BG);
+		modal.classList.add(this.CLASS_MODAL_SHOW);
 	},1500);
 }
+
+/*
+* 	Class: Star
+*	@constructor
+*	@description Represents a ranking star that indicates how well the player is doing
+*/
+const Star = function(parent){
+	this.parentNode = parent;
+	const e = document.createElement(this.SYMBOL_ELEMENT_TYPE);
+	e.className = this.CLASS_FA_STAR;
+	this.elem = document.createElement(this.ELEMENT_TYPE);
+	this.elem.className = this.CLASS_STAR;
+	this.elem.appendChild(e);
+};
+
+// static constants for the class
+Star.prototype.CLASS_FA_STAR = 'fa fa-star';
+Star.prototype.CLASS_STAR = 'star';
+Star.prototype.SYMBOL_ELEMENT_TYPE = 'i';
+Star.prototype.ELEMENT_TYPE = 'li';
+Star.prototype.CLASS_SELECTOR = '.star';
 
 /**
 * 	Class: Card
@@ -145,18 +202,19 @@ Scoreboard.prototype.stopGame = function () {
 *	elem - a DOM Element representing this card (a list item)
 * 	parentNode - A reference to the card's parent node - (the game board).
 */
-const Card = function (id, symbol, parent){
+const Card = function (symbol, parent){
 	this.parentNode = parent;
 	this.symbol = symbol;
 	this.state = this.STATE_HIDDEN; // init closed
 	const e = document.createElement(this.SYMBOL_ELEMENT_TYPE);
 	e.className = this.FA_PREFIX + symbol;
-	this.elem = document.createElement(this.CARD_ELEMENT_TYPE);
+	this.elem = document.createElement(this.ELEMENT_TYPE);
 	this.elem.className = this.CLASS_CARD;
 	this.elem.appendChild(e);
 };
 
 // static constants for the class.
+Card.prototype.CLASS_SELECTOR = '.card';
 Card.prototype.CLASS_CARD = 'card';
 Card.prototype.CLASS_OPEN = 'open';
 Card.prototype.CLASS_SHOW = 'show';
@@ -164,7 +222,7 @@ Card.prototype.STATE_SHOWING = 0;
 Card.prototype.STATE_HIDDEN = -1;
 Card.prototype.STATE_MATCHED = 1;
 Card.prototype.SYMBOL_ELEMENT_TYPE = 'i';
-Card.prototype.CARD_ELEMENT_TYPE = 'li';
+Card.prototype.ELEMENT_TYPE = 'li';
 Card.prototype.FA_PREFIX = 'fa fa-';
 
 
@@ -272,30 +330,26 @@ function createCards(parentNode) {
 		// create a pair of identical cards and save them
 		let type = Card.prototype.symbols[i];
 		cards.push(
-			new Card(i, type, parentNode),
-			new Card(i + len, type, parentNode)
+			new Card(type, parentNode),
+			new Card(type, parentNode)
 		);
 	}
 	return cards;
 }
 
 /*
-*  	@description Create an array of star elements
+*  	@description Create an array of three star elements
 *	@returns {Array} Elements of list items with a FA star as their class
 */
-function createStars() {
+function createStars(parentNode) {
 	const stars = [];
 	for (let i = 0; i < 3; i++){
-		// create the star element with the FA star image
-		let className = "fa fa-star";
-		let starElem = document.createElement('i');
-		starElem.className = className;
-		let listElem = document.createElement('li');
-		listElem.appendChild(starElem);
-		stars.push(listElem);
+		stars.push(new Star(parentNode));
 	}
 	return stars;
 }
+
+
 
 /*
 * 	@description randomly shuffles the elements of a given array
@@ -318,56 +372,50 @@ function shuffle(array) {
 }
 
 /**
+*	@description: remove old pieces (cards/stars) and add new for a new game
+*	@param pieces
+*/
+function initializeGameBoard(newObjects){
+	const oldElements = document.querySelectorAll(newObjects[0].CLASS_SELECTOR);
+
+	// remove the curently displayed star elements
+	// remove existing children displayed
+	oldElements.forEach(function(element){
+		element.parentElement.removeChild(element);
+	});
+
+	// append the new objects (cards or stars) to the document.
+	const fragment = document.createDocumentFragment();
+
+	// Build up the new elements to display
+	newObjects.forEach(function(object) {
+	    fragment.appendChild(object.elem);
+	});
+
+	// Add the new objects to the page.
+	newObjects[0].parentNode.appendChild(fragment);
+}
+
+/**
 * 	@description Set up the game by creating cards and shuffling them.
 *  	Then display the cards on the board and start the game.
-	@return {Array} an array of Card objects
 */
 function initialize() {
 
-	const deckFragment = document.createDocumentFragment();
-	const listFragment = document.createDocumentFragment();
-
-	// remove existing cards
-	while (deckNode.firstChild) {
-		deckNode.removeChild(deckNode.firstChild);
-	}
-	deckNode.addEventListener('click', respondToClick);
-
-	// remove existing stars
-	const starsToRemove = document.querySelectorAll('.stars li');
-	starsToRemove.forEach(function(star) {
-		star.parentElement.removeChild(star);
-	});
-
 	// Create a new deck of cards to display for the game
-	const cards = createCards(deckNode);
-	shuffle(cards);
+	cards = shuffle(createCards(deckNode));
+	// Create new stars to display for the game
+	stars = createStars(starList);
 
-	// Build up the card elements
-	cards.forEach(function(card) {
-	    deckFragment.appendChild(card.elem);
-	});
-
-	// Add the cards to the page.
-	deckNode.appendChild(deckFragment);
-
-	// create a new set of stars
-	const stars = createStars();
-
-	stars.forEach(function(star) {
-		listFragment.appendChild(star);
-	});
-
-	// append the stars to the document.
-	cardList.appendChild(listFragment);
+	// refresh the stars and cards
+	initializeGameBoard(stars);
+	initializeGameBoard(cards);
 
 	// reset the card to match.
 	cardToMatch = null;
 
 	// start a new game which reset the scoreboard
 	scoreboard.startGame();
-
-	return cards;
 }
 
 /**
@@ -381,16 +429,16 @@ function attemptMatch(card1, card2) {
 	} else {
 		// no match.
 		// don't let the player click on anything else while we display cards
-		deckNode.removeEventListener('click', respondToClick);
+		card1.parentNode.removeEventListener('click', respondToClick);
 		// hide the cards after 1.25 (animation takes .25) sec and start listening again for next move.
 		setTimeout(function (c1, c2) {
 			c1.hide();
 			c2.hide();
-			deckNode.addEventListener('click', respondToClick);
+			c1.parentNode.addEventListener('click', respondToClick);
 		}, 1250, card1, card2);
 	}
 	// we tried to match so increment the scoreboard and reset.
-	scoreboard.incrememntMoves();
+	scoreboard.incrementMoves();
 }
 
 /*
@@ -431,25 +479,25 @@ const respondToClick = function(evt) {
 }
 
 /**
+* 	@description  Closes the modal by removing the modal-show class name.
+* 	@param {EventListener}
+*/
+const closeModal = function(evt) {
+	if (modal.classList.contains(Scoreboard.prototype.CLASS_MODAL_SHOW)) {
+		modal.classList.remove(Scoreboard.prototype.CLASS_MODAL_SHOW);
+	}
+	if (modalBg.classList.contains(Scoreboard.prototype.CLASS_MODAL_SHOW_BG)) {
+		modalBg.classList.remove(Scoreboard.prototype.CLASS_MODAL_SHOW_BG);
+	}
+}
+
+/**
 * 	@description  Starts the game over clearing the scoreboard and reshuffling the deck
 * 	@param {EventListener}
 */
 const resetGame = function(evt) {
 	closeModal();
-	cards = initialize();
-}
-
-/**
-* 	@description  Closes the modal by removing the modal-show class name.
-* 	@param {EventListener}
-*/
-const closeModal = function(evt) {
-	if (modal.classList.contains('modal-show')) {
-		modal.classList.remove('modal-show');
-	}
-	if (modalBg.classList.contains('modal-bg-show')) {
-		modalBg.classList.remove('modal-bg-show');
-	}
+	initialize();
 }
 
 /**
@@ -475,7 +523,7 @@ const modalBg = document.querySelector('.modal-bg');
 /**
 * 	The List of stars on the scoreboard
 */
-const cardList = document.querySelector('.stars');
+const starList = document.querySelector('.stars');
 
 /**
 * 	The card selected by the player to compare for a match.
@@ -485,7 +533,15 @@ let cardToMatch = null;
 /**
 *	The set of cards for this game.
 */
-let cards = initialize();
+let cards = [];
+
+/**
+*	The set of stars for this game
+*/
+let stars = [];
+
+// add a global click listener on the board.
+deckNode.addEventListener('click', respondToClick);
 
 // listens for clicks on the restart buttons to begin a new game.
 document.querySelectorAll('.restart').forEach(function(btn) {
@@ -495,3 +551,5 @@ document.querySelectorAll('.restart').forEach(function(btn) {
 // listens for clicks on the little "x" on the modal window.
 document.querySelector(".modal-dismiss-btn").addEventListener('click', closeModal);
 
+// start the game
+initialize();
